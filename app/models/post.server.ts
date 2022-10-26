fa94bbf769ad9cad168d70887f6d048a698dc627
Viktor Sheverdin <32000453+ViktorSheverdin@users.noreply.github.com>
2022-10-25 23:02:20 -0700

    feat(quickstart): viktor/dynamic-routes (#3)
    Added dynamic routes and display markdown for each post

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,4 +1,5 @@
 import { prisma } from "~/db.server";
+export type { Post } from "@prisma/client";
 
 type Post = {
     slug: string;
@@ -7,4 +8,8 @@ type Post = {
 
 export async function getPosts(): Promise<Array<Post>> {
     return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+    return prisma.post.findUnique({ where: { slug } })
 }
