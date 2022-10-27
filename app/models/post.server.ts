a4d07078ee84e75af6aea56f2b307e901995fc06
Viktor Sheverdin <32000453+ViktorSheverdin@users.noreply.github.com>
2022-10-26 19:48:14 -0700

    feat(quickstart): viktor/nested-routing added nested routes and ability to create posts (#4)
    Added the nested routes, ability to create posts, and ensure Type safety

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,10 +1,6 @@
 import { prisma } from "~/db.server";
-export type { Post } from "@prisma/client";
-
-type Post = {
-    slug: string;
-    title: string;
-};
+import type { Post } from "@prisma/client";
+export type { Post };
 
 export async function getPosts(): Promise<Array<Post>> {
     return prisma.post.findMany();
@@ -12,4 +8,8 @@ export async function getPosts(): Promise<Array<Post>> {
 
 export async function getPost(slug: string) {
     return prisma.post.findUnique({ where: { slug } })
+}
+
+export async function createPost(post: Pick<Post, "slug" | "title" | "markdown">) {
+    return prisma.post.create({ data: post });
 }
